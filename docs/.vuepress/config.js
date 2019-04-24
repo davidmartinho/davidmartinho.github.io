b00e6278625140983f727d0891041083fe961293
David Martinho <davidmartinho@gmail.com>
2019-04-25 00:29:10 +0100

    Reset site to use Vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'routine.run',
+  description: 'David Martinho\'s Blog',
+  base: '/',
+  dest: 'public',
+  themeConfig: {
+      nav: [
+          { text: 'Home', link: '/' },
+      ]
+  }
+}
